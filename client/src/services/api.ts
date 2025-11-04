6f7596ce34998767713346f2c917e74e7ed06710
BlueBeagBB <gabrifelipegf@gmail.com>
2025-11-04 09:42:58 -0300

    feat(client): Atualiza baseURL da API para o ambiente de produção

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'curso-frontend-essencial-production.up.railway.app/api',
 } );
 
 api.interceptors.request.use(
